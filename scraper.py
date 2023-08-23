43581589d3ef633552afbfbff156522ad4a02f2a
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2023-08-23 12:39:04 +0100

    codes
    made modifications to the python code, added a while statement as well as a for loop

--- scraper.py
+++ scraper.py
@@ -1,3 +1,29 @@
 #Essential libraries import
 import requests
 from bs4 import BeautifulSoup
+
+quotes_list = []
+first_url = "https://quotes.toscrape.com"
+url = "/page/1"
+
+
+while url:
+    res = requests.get(f"{first_url}{url}")
+    print(f"Currently scrapping {first_url}{url}")
+    soup = BeautifulSoup(res.text, "html.parser")
+    quotes = soup.find_all(class_="quote")
+
+    # Looping through the quotes
+    for quote in quotes:
+        quotes_list.append({
+            "text":quote.find(class_="text").getText(),
+            "author":quote.find(class_="author").getText(),
+            "author-bio":quote.find("a")["href"]
+        })
+    #Moving to the next page
+    next_page = soup.find(class_="next")
+    url = next_page.find("a")["href"] if next_page else None
+
+# print(next_page.find("a")["href"])
+
+print(quotes_list)
